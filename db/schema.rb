d325fab8d637c524aafaa72359460e441f97f696
Rishav Rastogi <rrastogi@prosperworks.com>
2017-11-28 13:55:21 -0800

    Added Post Model and added ability for user to be admin

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171128213636) do
+ActiveRecord::Schema.define(version: 20171128215449) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -26,6 +34,7 @@ ActiveRecord::Schema.define(version: 20171128213636) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
